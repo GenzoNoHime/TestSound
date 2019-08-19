0df413aac859f5233c63d956e32fedf57f3679de
GenzoNoHime <49745025+GenzoNoHime@users.noreply.github.com>
2019-08-19 22:41:23 +0700

    Add files via upload

---
+++ TestApp/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1566229156,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"media/j006a.m4a",
+		"media/j006a.ogg",
+		"media/j032b.m4a",
+		"media/j032b.ogg",
+		"media/h030.m4a",
+		"media/h030.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
